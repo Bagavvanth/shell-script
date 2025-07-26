3aba9176740f9db19690bebee214390d9f03fb7a
bagavvanth <bagavvanth8@gmail.com>
2025-07-26 08:54:28 +0530

    logs in shell script

--- 13-logs.sh
+++ 13-logs.sh
@@ -7,9 +7,9 @@ G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
 LOGS_FOLDER="/var/log/shellscript-logs"
-SCRIPT_NAME=$(echo $0 | cut -d "." -f1 )
+LOG_NAME=$(echo $0 | cut -d "." -f1 )
 TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
-LOG_FILE_NAME="$LOGS_FOLDER/$SCRIPT_NAME/$TIMESTAMP.log"
+LOG_FILE_NAME="$LOGS_FOLDER/$LOG_NAME/$TIMESTAMP.log"
 
 VALIDATE(){
     
